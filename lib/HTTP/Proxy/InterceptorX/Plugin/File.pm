538544939a09e967c64006684284621da09190cd
Hernan Lopes <aut_hlopes@uolinc.com>
2013-11-26 21:35:52 -0200

    api changes

--- lib/HTTP/Proxy/InterceptorX/Plugin/File.pm
+++ lib/HTTP/Proxy/InterceptorX/Plugin/File.pm
@@ -6,15 +6,9 @@ our $VERSION = '0.01';
 
 use Moose::Role;
 
-=head2
-
-Plugin para abrir o conteudo de um arquivo e sobreescrever o conteúdo de uma url
-
-=cut
-
 sub File {
-    my ( $self, $args ) = @_; 
-    if ( exists $self->urls_to_proxy->{ $self->http_request->{ _uri }->as_string } && 
+    my ( $self, $args ) = @_;
+    if ( exists $self->urls_to_proxy->{ $self->http_request->{ _uri }->as_string } &&
          exists $self->urls_to_proxy->{ $self->http_request->{ _uri }->as_string }->{ File } ) {
         if ( -e $self->urls_to_proxy->{ $self->http_request->{ _uri }->as_string }->{ File } ) {
           $self->print_file_as_request( $self->urls_to_proxy->{ $self->http_request->{ _uri }->as_string }->{ File } );
@@ -27,7 +21,7 @@ sub File {
 }
 
 after 'BUILD'=>sub {
-    my ( $self ) = @_; 
+    my ( $self ) = @_;
     $self->append_plugin_method( "File" );
 };
 1;
@@ -37,15 +31,43 @@ __END__
 
 =head1 NAME
 
-HTTP::Proxy::InterceptorX::Plugin::File - Blah blah blah
+HTTP::Proxy::InterceptorX::Plugin::File - overwrite the response content with file content
 
 =head1 SYNOPSIS
 
-  use HTTP::Proxy::InterceptorX::Plugin::File;
+    package My::Custom::Proxy;
+    use Moose;
+    extends qw/HTTP::Proxy::Interceptor/;
+    with qw/
+        HTTP::Proxy::InterceptorX::Plugin::File
+    /;
+    1;
+
+    my $p = My::Custom::Proxy->new(
+      config_path => 'config_file.pl',
+      port        => 9919,
+    );
+
+    $p->start;
+    1;
+
+=head1 CONFIG
+
+create a config_file.pl
+
+    {
+        "http://some.site.com/some/url.htm" => {
+            File => "/home/user/files/some_file.htm"
+        },
+    }
+
+You will be point urls to Files 
+
+Start the proxy
 
 =head1 DESCRIPTION
 
-HTTP::Proxy::InterceptorX::Plugin::File is
+HTTP::Proxy::InterceptorX::Plugin::File allows you to point urls to local files and have the response content loaded from a local file.
 
 =head1 AUTHOR
 
